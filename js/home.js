33264cc44d6950cd47871c965d8c931d105c4224
Renaud <maladrix@hotmail.com>
2022-09-25 00:53:23 +0200

    filters update

--- js/home.js
+++ js/home.js
@@ -1,21 +1,18 @@
 /* https://restcountries.com/v3.1/all */
 let index = 0;
-let countryArray
-
+let countryArray;
 
 (async () => {
-    getData();
+   getData();
 })();
 
-
-
-function createCard(country){
-    let codePlace = document.getElementById("countryList")
-    let population = new Intl.NumberFormat("en-EN").format(country.population)
-    let div = document.createElement("div")
-    let capital = country.capital ? country.capital : ""
-    div.setAttribute("class", "country")
-    div.innerHTML = `<a href="./country.html?name=${country.name.common}">
+function createCard(country) {
+   let codePlace = document.getElementById("countryList");
+   let population = new Intl.NumberFormat("en-EN").format(country.population);
+   let div = document.createElement("div");
+   let capital = country.capital ? country.capital : "";
+   div.setAttribute("class", "country");
+   div.innerHTML = `<a href="./country.html?name=${country.name.common}">
                             <figure class="card">
                                 <img src=${country.flags.png} alt=${country.name.common}'s flag>
                                 <figcaption class="description">
@@ -26,82 +23,83 @@ function createCard(country){
                                 </figcaption>
                             </figure>
                             </a>
-                            `
-    codePlace.appendChild(div)
+                            `;
+   codePlace.appendChild(div);
 }
 
-
-async function getData(){
-    document.getElementById("countryList").innerHTML = ""
-    const response = await fetch("https://restcountries.com/v3.1/all")
-    data = await response.json()
-    countryArray = await sort(data)
-    for(let country of countryArray){
-        createCard(country)
-    }
+async function getData() {
+   document.getElementById("countryList").innerHTML = "";
+   const response = await fetch("https://restcountries.com/v3.1/all");
+   data = await response.json();
+   countryArray = await sort(data);
+   for (let country of countryArray) {
+      createCard(country);
+   }
 }
 
-
-function sort(countryArray){
-    let sort = countryArray.sort((a,b) =>{
-        return a.name.common < b.name.common ? -1 : 0
-    })
-    return sort;
+function sort(countryArray) {
+   let sort = countryArray.sort((a, b) => {
+      return a.name.common < b.name.common ? -1 : 0;
+   });
+   return sort;
 }
 
-function filterByRegion(value){
-    document.getElementById("countryList").innerHTML = ""
-    const filter = countryArray.filter(country => {return country.region.includes(value)})
-    for (let country of filter) {
-        createCard(country)
-    }
+function filterByRegion(value) {
+   document.getElementById("countryList").innerHTML = "";
+   const filter = countryArray.filter((country) => {
+      return country.region.includes(value);
+   });
+   for (let country of filter) {
+      createCard(country);
+   }
 }
 
-let select = document.querySelector("#select")
-select.addEventListener("click",() =>{
-   document.querySelector(".filterGroup").classList.toggle('none')
-})
+let select = document.querySelector("#select");
+select.addEventListener("click", () => {
+   document.querySelector(".filterGroup").classList.toggle("none");
+});
 
-let selectOption = document.querySelectorAll(".filter input")
-for(let filter of selectOption){
-    let choice = ""
-    filter.nextElementSibling.style.color = "white"
-    filter.addEventListener("click", (e) => {
-        if(e.target.checked){
-            choice = e.target.id
-            filterByRegion(e.target.name)
-            resetFilter(selectOption, choice)
-            filter.nextElementSibling.style.color = "red"
-        }
-        else{
-            filter.nextElementSibling.style.color = "white"
-            getData()
-        }
-        console.log("choice:" + choice)
-    })
+let selectOption = document.querySelectorAll(".filter input");
+for (let filter of selectOption) {
+   let choice = "";
+   filter.nextElementSibling.style.color = "white";
+   filter.addEventListener("click", (e) => {
+      if (e.target.checked) {
+         choice = e.target.id;
+         filterByRegion(e.target.name);
+         resetFilter(selectOption, choice);
+         filter.nextElementSibling.style.color = "red";
+      } else {
+         document.getElementById("countryList").innerHTML = "";
+         filter.nextElementSibling.style.color = "white";
+         for (let country of countryArray) {
+            createCard(country);
+         }
+      }
+   });
 }
 
-function resetFilter(selectOption, chosenFilter){
-    for(let option of selectOption){
-        if(option.id != chosenFilter){
-            option.nextElementSibling.style.color = "white"
-            option.checked = false
-        }
-    }
+function resetFilter(selectOption, chosenFilter) {
+   for (let option of selectOption) {
+      if (option.id != chosenFilter) {
+         option.nextElementSibling.style.color = "white";
+         option.checked = false;
+      }
+   }
 }
 
-function search(input){
-    document.getElementById("countryList").innerHTML = ""
-    let filter = countryArray.filter(country => {
-        return country.name.common.toLowerCase().includes(input)
-    })
-    for(let country of filter){
-        createCard(country)
-    }
+function search(input) {
+   document.getElementById("countryList").innerHTML = "";
+   let filter = countryArray.filter((country) => {
+      return country.name.common.toLowerCase().includes(input);
+   });
+   for (let country of filter) {
+      createCard(country);
+   }
 }
 
 let input = document.querySelector(".searchBar input");
 input.addEventListener("input", (e) => {
-    let string = e.target.value.toLowerCase()
-    search(string)
-})
+   let string = e.target.value.toLowerCase();
+   search(string);
+});
--- themeHandler.js
+++ themeHandler.js
@@ -1,50 +1,45 @@
-let button = document.getElementById("themeSwitch")
-let body = document.querySelector("body")
-let lightOn = body.classList.contains("light")
+let button = document.getElementById("themeSwitch");
+let body = document.querySelector("body");
+let lightOn = body.classList.contains("light");
 
-let dark = '<i class="fa-solid fa-moon"></i> Dark Mode'
-let light = '<i class="fa-regular fa-moon"></i> Light Mode'
-setTheme()
-console.log(lightOn)
+let dark = '<i class="fa-solid fa-moon"></i> Dark Mode';
+let light = '<i class="fa-regular fa-moon"></i> Light Mode';
+setTheme();
 button.addEventListener("click", () => {
-    document.querySelector("body").classList.toggle("light")
-    lightOn = !lightOn;
-    setInLocal(lightOn)
-    !lightOn ? button.innerHTML= light : button.innerHTML= dark
-    playSound(lightOn)
-})
+   document.querySelector("body").classList.toggle("light");
+   lightOn = !lightOn;
+   setInLocal(lightOn);
+   !lightOn ? (button.innerHTML = light) : (button.innerHTML = dark);
+   playSound(lightOn);
+});
 
-function setInLocal(light){
-    localStorage.setItem("lightTheme", light)
+function setInLocal(light) {
+   localStorage.setItem("lightTheme", light);
 }
 
-function getTheme(){
-    return localStorage.getItem("lightTheme")
+function getTheme() {
+   return localStorage.getItem("lightTheme");
 }
 
-function setTheme(){
-    let state = getTheme()
-    console.log("state:" + state)
-    if(state && state == "true"){
-        body.classList.add("light")
-        lightOn = true
-    }
-    else{
-        body.classList.remove("light")
-        lightOn = false
-    }
-    !lightOn ? button.innerHTML= light : button.innerHTML= dark
+function setTheme() {
+   let state = getTheme();
+   if (state && state == "true") {
+      body.classList.add("light");
+      lightOn = true;
+   } else {
+      body.classList.remove("light");
+      lightOn = false;
+   }
+   !lightOn ? (button.innerHTML = light) : (button.innerHTML = dark);
 }
 
-
-function playSound(sound){
-    let name = ""
-    if(sound){
-        name = "Jour"
-    }
-    else{
-        name = "Nuit"
-    }
-    var audio = new Audio("./assets/" + name + ".mp3")
-    audio.play()
+function playSound(sound) {
+   let name = "";
+   if (sound) {
+      name = "Jour";
+   } else {
+      name = "Nuit";
+   }
+   var audio = new Audio("./assets/" + name + ".mp3");
+   audio.play();
 }
